93dd637a688581b3263403728eb72efc432e1047
pmbrull <pere.miquel.brull.borras@zurich.com>
2020-09-26 17:03:55 +0200

    :sparkles: Update some pages

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -3,26 +3,19 @@
     <div>
       <h1 class="inline text-4xl lg:text-5xl text-gray-800 font-bold">Hola!</h1>
       <p class="inline ml-4 lg:text-2xl text-gray-600">\ɔɫə\</p>
-      <p class="font-medium text-gray-700">
-        Welcome to my playground! 👋
-      </p>
+      <p class="font-medium text-gray-700">Welcome to my playground! 👋</p>
       <div class="text-sm text-gray-700 mt-2">
-      <p>
-         Here you will find {{ bloglistLen }} different posts (so far!).
-      </p>
-      <p>
-        I like to write about
-        Big Data, Cloud, ML, Python, Spark and Scala.
-      </p>
+        <p>Here you will find {{ bloglistLen }} different posts (so far!).</p>
+        <p>I like to write about Big Data, Cloud, ML, Python, Spark and Scala.</p>
       </div>
-      
+
       <!-- <p class="font-medium text-gray-700">
         <ul class="align-center justify-center">
           <p v-for="cat in categories" :key="cat">
            {{ cat }} - {{ postsPerCat(cat) }}
           </p>
         </ul>
-      </p> -->
+      </p>-->
     </div>
   </section>
 </template>
@@ -31,21 +24,22 @@
 export default {
   data() {
     return {
-      categories: ['Big Data', 'Cloud', 'ML', 'Python', 'Scala', 'Spark']
-    }
+      categories: ["Big Data", "Cloud", "ML", "Python", "Scala", "Spark"],
+    };
   },
   computed: {
     bloglistLen() {
-      return this.bloglist().length
-    }
+      return this.bloglist().length;
+    },
   },
   methods: {
     bloglist() {
-      return this.$store.state.bloglist
+      return this.$store.state.bloglist;
     },
     postsPerCat(category) {
-      return this.bloglist().filter(post => post.category === category).length
-    }
-  }
+      return this.bloglist().filter((post) => post.category === category)
+        .length;
+    },
+  },
 };
 </script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,68 +1,65 @@
 <template>
-  <div class="container mx-auto px-16">      
-    <h1 class="font-bold text-4xl text-gray-800 px-3 mt-6 mb-4 text-center">
-      About me and this site...
-    </h1>  
-      <section class="p-4">
-        <div class="flex flex-wrap -mx-10 justify-between">
-          <div class="md:w-1/3 mt-12 w-full p-6">
-            <img :src="require(`~/assets/${maths}`)"
-                  alt="feeling"
-                  class="h-48 justify-center m-auto">
-            <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center">
-              As a Mathematician, everything is a puzzle! And working with data we have
-              a ton of fancy pieces to solve what we have at our hands.
-            </p>
-          </div>
-          <div class="md:w-1/3 mt-12 w-full p-6 m-auto pt-12">
-            <img :src="require(`~/assets/${world}`)"
-                  alt="world"
-                  class="h-40 justify-center m-auto">
-            <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center pt-2">
-              I always try to make our world a bit better. Let's recycle, share, smile
-              and have fun! Let's make a change, we're people, not resources.
-            </p>
-          </div>
-          <div class="md:w-1/3 mt-12 w-full p-6 m-auto">
-            <img :src="require(`~/assets/${code}`)"
-                  alt="code"
-                  class="h-48 justify-center m-auto">
-            <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center">
-              ... I just love coding! And with this site I'd want to share my experiences
-              and work with all of you.
-            </p>
-          </div>
+  <div class="container mx-auto px-16">
+    <h1
+      class="font-bold text-4xl text-gray-800 px-3 mt-6 mb-4 text-center"
+    >About me and this site...</h1>
+    <section class="p-4">
+      <div class="flex flex-wrap -mx-10 justify-between">
+        <div class="md:w-1/3 mt-12 w-full p-6">
+          <img :src="require(`~/assets/${maths}`)" alt="feeling" class="h-48 justify-center m-auto" />
+          <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center">
+            As a Mathematician, everything is a puzzle! And working with data we have
+            a ton of fancy pieces to solve what we have at our hands.
+          </p>
         </div>
+        <div class="md:w-1/3 mt-12 w-full p-6 m-auto pt-12">
+          <img :src="require(`~/assets/${world}`)" alt="world" class="h-40 justify-center m-auto" />
+          <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center pt-2">
+            I always try to make our world a bit better. Let's recycle, share, smile
+            and have fun! Let's make a change, we're people, not resources.
+          </p>
+        </div>
+        <div class="md:w-1/3 mt-12 w-full p-6 m-auto">
+          <img :src="require(`~/assets/${code}`)" alt="code" class="h-48 justify-center m-auto" />
+          <p class="mt-1 text-sm text-gray-700 mt-6 m-auto text-center">
+            ... I just love coding! And with this site I'd want to share my experiences
+            and work with all of you.
+          </p>
+        </div>
+      </div>
 
       <div>
         <p class="mt-1 text-sm text-gray-700 mt-6 m-auto display-inline text-center">
           Finally, I am really curious and passionate about learning. I enjoy looking for new and elegant ways
-          to achieve whatever I am working on, both at work and at home. <em>Let's push it to the limit! 
-          Have we tried that? </em>
+          to achieve whatever I am working on, both at work and at home.
+          <em>
+            Let's push it to the limit!
+            Have we tried that?
+          </em>
         </p>
         <p class="mt-1 text-sm text-gray-700 mt-6 m-auto display-inline text-center">
           With this blog I'll try to keep my head and ideas organized, while also being able to give back
           to the internet community a bit of content just in return for the thousands of hours spent in Stack Overflow!
+          Most of the content you will find here though are my notes and diggests on online courses and learning resources,
+          as taking notes has helped my study since forever.
         </p>
       </div>
 
       <div>
         <p class="mt-1 text-sm text-gray-700 mt-6 m-auto display-inline text-center">
-         Regarding the blog itself... I started playing around with blogs around 2018.
-         I started with a simple Jekyll theme hosted in Github pages, but it lacked the freedom
-         I wanted to achieve. By then I was really in love with Scala - I still am - so I
-         switched to Play Framework just pour le fun. I was happy with the result but I did not
-         integrate any markdown in it as I wanted to mess with ScalaJS. In the end, writing
-         was a bit cumbersome, so I thought about another change. I played a bit with a 47 Degree's
-         project called microsites, which was really fast to spin up. But again, it did not
-         offer much flexibility.
-
-         Now, I just wanted to do something that i'd like the look and feel but that would
-         be easy to push me to write new content. Therefore, I went directly to Javascript with
-         VueJS and a Markdown plugin.
-
-         I am no frontend developer, and it was not easy to get to this point, but it was indeed
-         really fun. I hope you enjoy this as much as I did during all these years! 😍
+          Regarding the blog itself... I started playing around with blogs around 2018.
+          I started with a simple Jekyll theme hosted in Github pages, but it lacked the freedom
+          I wanted to achieve. By then I was really in love with Scala - I still am - so I
+          switched to Play Framework just pour le fun. I was happy with the result but I did not
+          integrate any markdown in it as I wanted to mess with ScalaJS. In the end, writing
+          was a bit cumbersome, so I thought about another change. I played a bit with a 47 Degree's
+          project called microsites, which was really fast to spin up. But again, it did not
+          offer much flexibility.
+          Now, I just wanted to do something that i'd like the look and feel but that would
+          be easy to push me to write new content. Therefore, I went directly to Javascript with
+          VueJS and a Markdown plugin.
+          I am no frontend developer, and it was not easy to get to this point, but it was actually
+          really fun. I hope you enjoy this as much as I did during all these years! 😍
         </p>
       </div>
 
@@ -72,42 +69,40 @@
           <ul class="list-disc">
             <li>
               The SVG illustrations come from
-              <a rel="noopener" 
+              <a
+                rel="noopener"
                 class="font-semibold hover:underline text-gray-700"
-                href="https://undraw.co/illustrations">
-                unDraw
-              </a>
+                href="https://undraw.co/illustrations"
+              >unDraw</a>
             </li>
             <li>
               Profile banner is by Fabrizio Conti from
-              <a rel="noopener" 
+              <a
+                rel="noopener"
                 class="font-semibold hover:underline text-gray-700"
-                href="https://unsplash.com">
-                Unsplash
-              </a>
+                href="https://unsplash.com"
+              >Unsplash</a>
             </li>
           </ul>
           <p class="mt-4">Thanks for the coolest content!</p>
         </div>
       </div>
-        
-      </section>
-    </div>
+    </section>
+  </div>
 </template>
 
 <script>
 // import SiteHero from "~/components/SiteHero";
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
       feeling: "images/about/feeling.svg",
       world: "images/about/world.svg",
       code: "images/about/code.svg",
-      maths: "images/about/maths.svg"
-    }
-  }
+      maths: "images/about/maths.svg",
+    };
+  },
 };
 </script>
 
